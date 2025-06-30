6b2df0b617db6bb3b7102e5ec068cafcd86948c1
Fahami Jemal <fahamijemal1@gmail.com>
2025-07-01 00:14:45 +0300

    add User model with validation for name, email, and password

--- models/user.model.js
+++ models/user.model.js
@@ -1,26 +1,30 @@
 import mongoose from "mongoose";
-const userSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, 'User name is required'],
-        minlength:2,
-        maxlength:50,
-     },
-     email:{
-        type:String,
-        required:[true, 'User email is required'],
-        unique:true,
-        trim:true,
-        lowercase:true,
-        match:/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-     },
-     password:{
-        type:String,
-        required:[true, 'User password is required'],
-        minlength:6,
 
-     }
-},{timestamps:true});
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'User name is required'],
+    minlength: 2,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, 'User email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [
+      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+      'Please provide a valid email address',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'User password is required'],
+    minlength: 6,
+  }
+}, { timestamps: true });
 
-const user= mongoose.model('User',userSchema);
-export default user;
+// Create User model
+const User = mongoose.model('User', userSchema);
+export default User;
