527c030be9d8c88b3e3c2529189d9a392f54c0c5
Fahami Jemal <fahamijemal1@gmail.com>
2025-07-20 06:36:19 +0300

    feat: integrate new features into main application
    - Add workflow routes to main app
    - Update subscription routes with enhanced functionality
    - Integrate Arcjet middleware (commented for development)
    - Add proper route organization and error handling

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,25 +1,31 @@
 import { Router } from "express";
+import authorize from '../middlewares/auth.middleware.js';
+import {createSubscription, getUserSubscriptions} from "../controllers/subscription.controller.js";
+
+
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.get('/', (req, res) => {
     res.send({ title: 'Get all subscriptions' });
 });
+
 subscriptionRouter.get('/:id', (req, res) => {
     res.send({ title: 'Get subscription by ID' });
 });
-subscriptionRouter.post('/', (req, res) => {
-    res.send({ title: 'Create subscription' });
-});
+
+subscriptionRouter.post('/', authorize, createSubscription);
+
 subscriptionRouter.put('/:id', (req, res) => {
     res.send({ title: 'Update the subscription' });
 });
+
 subscriptionRouter.delete('/:id', (req, res) => {
     res.send({ title: 'Delete the subscription' });
 });
-subscriptionRouter.get('/user/:id', (req, res) => {
-    res.send({ title: 'Get all the user subscriptions' });
-});
+
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions );
+
 subscriptionRouter.put('/:id/cancel', (req, res) => {
     res.send({ title: 'Cancel the subscription' });
 });
